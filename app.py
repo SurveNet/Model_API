45085be0d06bd2e158a7f684024a9613075ae8cb
AaronWard <aaronlaptop12@hotmail.com>
2017-12-08 23:10:17 +0000

    added get request

--- app.py
+++ app.py
@@ -26,44 +26,31 @@ def home():
     response = "Route index for Survenet model API"
     return jsonify(result= response)
 
-@app.route('/model',methods=['POST'])
+@app.route('/model',methods=['GET','POST'])
 def make_a_prediction():
-    print('======= 1 Request made by surveyapp =========')
+    print('1: Request made by surveyapp')
 
     imageData = request.get_data()
     convertImage(imageData)
 
-    print('======= 2 GOT PAST CONVERTIMAGE =========')
+    print('2: Image was converted')
 
-    # img = imread('output.jpg',mode='L')
-
-    # print('======= 3 =========')
-    
-    # image = np.invert(image)
-    
-    # print('======= 4 =========')
-
-    # x = imresize(x, (64, 64))
-
-    # print('======= 5 =========')
-
-
-    # x = x.reshape(64, 64, 3)
     test_image = image.load_img('output.jpg',target_size=(64, 64))
     test_image = image.img_to_array(test_image)
+
+    print('3: image converted to array')
     test_image = np.expand_dims(test_image, axis = 0)
-    print('======= 6 =========')
+    print('4: Dimension added')
 
-    with graph.as_default():
-        result = model.predict(test_image)
+    # with graph.as_default():
+    result = model.predict(test_image)
+    print(result[0][0])
 
-        print(result[0][0])
-
-        if result[0][0] > 0.5:
-            response = 'Happy'
-        else:
-            response = 'Sad'
-        return response
+    if result[0][0] > 0.5:
+        response = 'Happy'
+    else:
+        response = 'Sad'
+    return response
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6000))
