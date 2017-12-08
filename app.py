f816f87c137e6e63535ecac396b05a8c5ea2b785
AaronWard <aaronlaptop12@hotmail.com>
2017-12-08 21:57:59 +0000

    Added axis, return response

--- app.py
+++ app.py
@@ -45,9 +45,9 @@ def make_a_prediction():
 
     print('======= 5 =========')
 
-    x = x.reshape(64, 64, 1)
+    x = x.reshape(-1, 64, 64)
 
-    x = np.expand_dims(x, axis = -1)
+    x = np.expand_dims(x, axis = 3)
     print('======= 6 =========')
 
     with graph.as_default():
